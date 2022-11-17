68a54d1fc15f505dd65cf089922a54cf0c6684d0
alienzj <alienchuj@gmail.com>
2022-11-17 10:19:04 +0800

    update for magic

---
+++ hosts/magic/default.nix
@@ -0,0 +1,148 @@
+{ pkgs, config, lib, ... }:
+{
+  imports = [
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  ## Modules
+  modules = {
+    desktop = {
+      bspwm.enable = true;
+      apps = {
+        rofi.enable = true;
+	skype.enable = true;
+	zoomus.enable = true;
+	libreoffice.enable = true;
+        # godot.enable = true;
+      };
+      browsers = {
+        default = "brave";
+        brave.enable = true;
+        firefox.enable = true;
+        qutebrowser.enable = true;
+      };
+      gaming = {
+        steam.enable = true;
+        #emulators.enable = true;
+        #emulators.psx.enable = true;
+      };
+      media = {
+        daw.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+        recording.enable = true;
+        spotify.enable = true;
+      };
+      term = {
+        default = "xst";
+        st.enable = true;
+      };
+      vm = {
+        qemu.enable = true;
+      };
+      noter = {
+	zotero.enable = true;
+	calibre.enable = true;
+      };
+      im = {
+        matrix.enable = true;
+	tdesktop.enable = true;
+	whatsapp.enable = true;
+	slack.enable = true;
+      };
+    };
+    dev = {
+      cc.enable = true;
+      node.enable = true;
+      rust.enable = true;
+      python.enable = true;
+    };
+    editors = {
+      default = "nvim";
+      emacs.enable = true;
+      vim.enable = true;
+      vscode.enable = true;
+    };
+    shell = {
+      adl.enable = true;
+      vaultwarden.enable = true;
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      tmux.enable   = true;
+      zsh.enable    = true;
+    };
+    services = {
+      syncthing.enable = true;
+      ssh.enable = true;
+      docker.enable = true;
+      # Needed occasionally to help the parental units with PC problems
+      teamviewer.enable = true;
+      adb.enable = true; # android
+      rdp.enable = true; # remote desktop
+    };
+    utils = {
+      htop.enable = true;
+      neofetch.enable = true;
+    };
+    theme.active = "alucard";
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  networking.networkmanager.enable = true;
+
+
+  ## Personal backups
+  # Syncthing is a bit heavy handed for my needs, so rsync to my NAS instead.
+  #systemd = {
+  #  services.backups = {
+  #    description = "Backup /usr/store to NAS";
+  #    wants = [ "usr-drive.mount" ];
+  #    path  = [ pkgs.rsync ];
+  #    environment = {
+  #      SRC_DIR  = "/usr/store";
+  #      DEST_DIR = "/usr/drive";
+  #    };
+  #    script = ''
+  #      rcp() {
+  #        if [[ -d "$1" && -d "$2" ]]; then
+  #          echo "---- BACKUPING UP $1 TO $2 ----"
+  #          rsync -rlptPJ --chmod=go= --delete --delete-after \
+  #              --exclude=lost+found/ \
+  #              --exclude=@eaDir/ \
+  #              --include=.git/ \
+  #              --filter=':- .gitignore' \
+  #              --filter=':- $XDG_CONFIG_HOME/git/ignore' \
+  #              "$1" "$2"
+  #        fi
+  #      }
+  #      rcp "$HOME/projects/" "$DEST_DIR/projects"
+  #      rcp "$SRC_DIR/" "$DEST_DIR"
+  #    '';
+  #    serviceConfig = {
+  #      Type = "oneshot";
+  #      Nice = 19;
+  #      IOSchedulingClass = "idle";
+  #      User = config.user.name;
+  #      Group = config.user.group;
+  #    };
+  #  };
+  #  timers.backups = {
+  #    wantedBy = [ "timers.target" ];
+  #    partOf = [ "backups.service" ];
+  #    timerConfig.OnCalendar = "*-*-* 00,12:00:00";
+  #    timerConfig.Persistent = true;
+  #  };
+  #};
+
+  time.timeZone = "Asia/Hong_Kong";
+
+  programs.adb.enable = true;
+
+}
