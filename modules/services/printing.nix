1dabd30963089a84b692285e2f0e6fa8ec1ac3cb
alienzj <alienchuj@gmail.com>
2022-11-20 16:30:31 +0800

    save

--- modules/services/printing.nix
+++ modules/services/printing.nix
@@ -3,6 +3,7 @@
 with lib;
 with lib.my;
 let cfg = config.modules.services.printing;
+    configDir = config.dotfiles.configDir;
 in {
   options.modules.services.printing = {
     enable = mkBoolOpt false;
@@ -13,6 +14,9 @@ in {
       services.printing = {
         enable = true;
         #drivers = [ pkgs.epson-escpr ];
+	#drivers = [
+        #  (writeTextDir "share/cups/model/Ricoh/Ricoh-IM_C6000-PDF-Ricoh.ppd" (builtins.readFile "${configDir}/printer/Ricoh-IM_C6000-PDF-Ricoh.ppd"))
+        #];
       };
     }
   ]);
