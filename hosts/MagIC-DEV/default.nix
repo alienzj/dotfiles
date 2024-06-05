5f48f44adc78e6b7a9eef8f0b4983ded83cfaa88
Jie Zhu <alienchuj@gmail.com>
2024-06-05 15:41:35 +0800

    add MagIC-DEV

---
+++ hosts/MagIC-DEV/default.nix
@@ -0,0 +1,81 @@
+# MagIC-APP -- MagIC APP server
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib.my;
+{
+  imports = [
+    ../server.nix
+    ../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  # Modules
+  modules = {
+    dev = {
+      cc = {
+        enable = true;
+        xdg.enable = true;
+      };
+      node = {
+        enable = true;
+        xdg.enable = true;
+      };
+      rust = {
+        enable = true;
+        xdg.enable = true;
+      };
+      python = {
+        enable = true;
+        xdg.enable = true;
+      };
+      shell = {
+        enable = true;
+        xdg.enable = true;
+      };
+      web.enable = true;
+    };
+    editors = {
+      default = "nvim";
+      vim.enable = true;
+    };
+    shell = {
+      direnv.enable = true;
+      git.enable = true;
+      gnupg.enable = true;
+      tmux.enable = true;
+      zsh.enable = true;
+      fish.enable = true;
+    };
+    services = {
+      fail2ban.enable = true;
+      ssh.enable = true;
+      nginx.enable = true;
+      docker.enable = true;
+      earlyoom.enable = true;
+      syncthing.enable = true;
+    };
+    utils = {
+      htop.enable = true;
+      neofetch.enable = true;
+      pandoc.enable = true;
+      ghostscript.enable = true;
+      disk.enable = true;
+    };
+  };
+
+  # Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  # Run unpatched dynamic binaries on NixOS
+  programs.nix-ld.enable = true;
+
+  ## Security
+  #security.acme.defaults.email = "alienchuj@gmail.com";
+}
