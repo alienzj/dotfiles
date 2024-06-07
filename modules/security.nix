8ed6772c485bdabf608e72bb583e8d449a251a0d
Jie Zhu <alienchuj@gmail.com>
2024-06-07 10:20:31 +0800

    increase tmp size

--- modules/security.nix
+++ modules/security.nix
@@ -13,6 +13,7 @@
   # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
   # on ssd systems, and volatile! Because it's wiped on reboot.
   boot.tmp.useTmpfs = lib.mkDefault true;
+  boot.tmp.tmpfsSize = "80%"; # avoid no space left when rebuild
   # If not using tmpfs, which is naturally purged on reboot, we must clean it
   # /tmp ourselves. /tmp should be volatile storage!
   boot.tmp.cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
