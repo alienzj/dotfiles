fc8964852f3042d42c0dd779635d48bd9489d42d
Jie Zhu <alienchuj@gmail.com>
2024-06-12 13:21:22 +0800

    update hedgedoc

--- modules/services/hedgedoc.nix
+++ modules/services/hedgedoc.nix
@@ -43,20 +43,21 @@ in {
               port = cfg.port;
 
               # CMD_DOMAIN
-              #domain = "localhost";
+              domain = "localhost";
               # CMD_PROTOCOL_USESSL
               # only applied when domain is set
-              #protocolUseSSL = true;
+              protocolUseSSL = true;
 
               # CMD_URL_ADDPORT
               # set to add port on callback URL (ports 80 or 443 won't be applied)
               # only applied when domain is set
-              #urlAddPort = true;
+              urlAddPort = true;
 
               # CMD_ALLOW_ORIGIN
-              #allowOrigin = [
-              #  "localhost"
-              #];
+              allowOrigin = [
+                "localhost"
+                cfg.host
+              ];
 
               # CMD_ALLOW_GRAVATAR
               allowGravatar = true;
