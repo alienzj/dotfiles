45ff149d0e842fb4fd19f26d8148c7432a80b2e3
Jie Zhu <alienchuj@gmail.com>
2024-06-17 16:31:13 +0800

    conda works

--- hosts/magic/default.nix
+++ hosts/magic/default.nix
@@ -123,7 +123,11 @@
       go.enable = true;
       haskell.enable = true;
       zeal.enable = true;
-      conda.enable = true;
+      conda = {
+        enable = true;
+        installationPath = "/home/alienzj/.conda/envs/env-base";
+        extraPkgs = [pkgs.gcc];
+      };
       ruby.enable = true;
       web.enable = true;
     };
--- modules/dev/conda.nix
+++ modules/dev/conda.nix
@@ -15,7 +15,14 @@ in {
   options.modules.dev.conda = with types; {
     enable = mkBoolOpt false;
     installationPath = mkOpt types.str "~/.conda/envs/env-base";
-    extraPkgs = mkOpt types.list [];
+    extraPkgs = mkOption {
+      type = with types; listOf package;
+      default = [pkgs.gcc];
+      example = literalExpression "with pkgs; [ gcc ]";
+      description = ''
+        Enabled extra pkgs in conda environment.
+      '';
+    };
   };
 
   config = mkIf cfg.enable (mkMerge [
