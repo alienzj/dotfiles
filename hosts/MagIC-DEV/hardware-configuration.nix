3649668e7d8f41e6a4549dc916948929ec82cc69
Jie Zhu <alienchuj@gmail.com>
2024-06-18 16:36:45 +0800

    test

--- hosts/MagIC-DEV/hardware-configuration.nix
+++ hosts/MagIC-DEV/hardware-configuration.nix
@@ -13,6 +13,9 @@
     initrd.kernelModules = ["cifs"];
     kernelModules = [];
     extraModulePackages = [];
+    kernel.sysctl = {
+      "unprivileged_userns_clone" = 1; # for conda and mamba
+    };
   };
 
   # Display
