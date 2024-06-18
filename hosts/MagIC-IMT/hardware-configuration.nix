9e7bf48cf65b95a38f82fa1d57258e4dc1e9e05c
Jie Zhu <alienchuj@gmail.com>
2024-06-18 17:33:13 +0800

    begin to add host MagIC-IMT

---
+++ hosts/MagIC-IMT/hardware-configuration.nix
@@ -0,0 +1,83 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [];
+
+  # Kernel
+  boot = {
+    initrd.availableKernelModules = ["ata_piix" "vmw_pvscsi" "sd_mod" "sr_mod"];
+    initrd.kernelModules = ["cifs"];
+    kernelModules = [];
+    extraModulePackages = [];
+  };
+
+  # Display
+  services.xserver.videoDrivers = ["vmware"];
+
+  # Virtualisation
+  virtualisation.vmware.guest.enable = true;
+
+  services.logrotate.checkConfig = false;
+
+  # Filesystem
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/BOOT";
+      fsType = "vfat";
+    };
+    "/srv" = {
+      device = "/dev/disk/by-label/DATA";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+  };
+
+  # Networking
+  networking = {
+    domain = "magic.local";
+    nameservers = ["1.1.1.1" "8.8.8.8" "10.132.2.30" "10.132.2.31"];
+
+    #networkmanager.enable = true;
+
+    firewall.enable = true;
+
+    # Network
+    interfaces.ens192 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "10.132.2.71";
+          prefixLength = 24;
+        }
+      ];
+    };
+    # DATA
+    interfaces.ens224 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.1.212";
+          prefixLength = 24;
+        }
+      ];
+    };
+
+    # Gateway
+    defaultGateway = {
+      address = "10.132.2.254";
+      interface = "ens192";
+    };
+  };
+
+  # Platform
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
