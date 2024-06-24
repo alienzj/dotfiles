cf78fd2fa8d3207ae8afd9bd9418341b19d5e142
Jie Zhu <alienchuj@gmail.com>
2024-06-24 11:11:04 +0800

    update R related configuration

--- modules/dev/r.nix
+++ modules/dev/r.nix
@@ -8,79 +8,25 @@
 with lib;
 with lib.my; let
   cfg = config.modules.dev.r;
-
-  curatedMetagenomicData_ = pkgs.rPackages.buildRPackage {
-    name = "curatedMetagenomicData";
-    src = pkgs.fetchFromGitHub {
-      owner = "waldronlab";
-      repo = "curatedMetagenomicData";
-      rev = "a8229bf3eaf8c9992838286e58825a32a0f0316";
-      #sha256 = lib.fakeSha256;
-      hash = "sha256-krRJ/7bZMhKJ0ptOp01MY7PBj1d38+FCMPGkvgTnkO0=";
-    };
-    propagatedBuildInputs = [
-      pkgs.unstable.rPackages.SummarizedExperiment
-      pkgs.unstable.rPackages.TreeSummarizedExperiment
-      pkgs.unstable.rPackages.AnnotationHub
-      pkgs.unstable.rPackages.ExperimentHub
-      pkgs.unstable.rPackages.S4Vectors
-      pkgs.unstable.rPackages.dplyr
-      pkgs.unstable.rPackages.magrittr
-      pkgs.unstable.rPackages.mia
-      pkgs.unstable.rPackages.purrr
-      pkgs.unstable.rPackages.rlang
-      pkgs.unstable.rPackages.stringr
-      pkgs.unstable.rPackages.tibble
-      pkgs.unstable.rPackages.tidyr
-      pkgs.unstable.rPackages.tidyselect
-      pkgs.unstable.rPackages.DirichletMultinomial
-    ];
-    nativeBuildInputs = [
-      pkgs.unstable.R
-      #  pkgs.unstable.rPackages.knitr
-      #  pkgs.unstable.pkg-config
-      #  pkgs.unstable.gsl
-      #  pkgs.unstable.make
-    ];
-    buildInputs = [pkgs.unstable.gsl];
-  };
-
   r-with-packages = pkgs.unstable.rWrapper.override {
     packages = with pkgs.unstable.rPackages; [
       tidyverse
-      # library(tidyverse) will load the core tidyverse packages:
-      ## ggplot2, for data visualisation.
-      ## dplyr, for data manipulation.
-      ## tidyr, for data tidying.
-      ## readr, for data import.
-      ## purrr, for functional programming.
-      ## tibble, for tibbles, a modern re-imagining of data frames.
-      ## stringr, for strings.
-      ## forcats, for factors.
-      ## lubridate, for date/times.
-
       tidymodels
       infer
-
       devtools
       remotes
-
       feather
       httr
       jsonlite
       xml2
       stringi
       curl
-
       shiny
-
       knitr
       rmarkdown
       tinytex
       ymlthis
-
       vegan
-
       ggtree
       ggtreeExtra
       tidytree
@@ -88,16 +34,13 @@ with lib.my; let
       MicrobiomeProfiler
       clusterProfiler
       enrichplot
-
       dada2
       DECIPHER
-
       ggpubr
       ggplotify
       ggalluvial
       ggstar
       ggnewscale
-
       coin
       forcats
       writexl
@@ -105,10 +48,8 @@ with lib.my; let
       randomForest
       curatedMetagenomicData
       SummarizedExperiment
-
       Rcpp
       Rcpp11
-
       Maaslin2
       pkgconfig
       ComplexHeatmap
@@ -118,7 +59,6 @@ with lib.my; let
       yaml
       optparse
       glue
-
       V8
       languageserver
     ];
--- modules/editors/rstudio.nix
+++ modules/editors/rstudio.nix
@@ -8,77 +8,25 @@
 with lib;
 with lib.my; let
   cfg = config.modules.editors.rstudio;
-
-  curatedMetagenomicData_ = pkgs.rPackages.buildRPackage {
-    name = "curatedMetagenomicData";
-    src = pkgs.fetchFromGitHub {
-      owner = "waldronlab";
-      repo = "curatedMetagenomicData";
-      rev = "a8229bf3eaf8c9992838286e58825a32a0f0316";
-      #sha256 = lib.fakeSha256;
-      hash = "sha256-krRJ/7bZMhKJ0ptOp01MY7PBj1d38+FCMPGkvgTnkO0=";
-    };
-    propagatedBuildInputs = [
-      pkgs.unstable.rPackages.SummarizedExperiment
-      pkgs.unstable.rPackages.TreeSummarizedExperiment
-      pkgs.unstable.rPackages.AnnotationHub
-      pkgs.unstable.rPackages.ExperimentHub
-      pkgs.unstable.rPackages.S4Vectors
-      pkgs.unstable.rPackages.dplyr
-      pkgs.unstable.rPackages.magrittr
-      pkgs.unstable.rPackages.mia
-      pkgs.unstable.rPackages.purrr
-      pkgs.unstable.rPackages.rlang
-      pkgs.unstable.rPackages.stringr
-      pkgs.unstable.rPackages.tibble
-      pkgs.unstable.rPackages.tidyr
-      pkgs.unstable.rPackages.tidyselect
-      pkgs.unstable.rPackages.DirichletMultinomial
-    ];
-    nativeBuildInputs = [
-      pkgs.unstable.R
-      #pkgs.unstable.rPackages.rlang
-      #pkgs.unstable.rPackages.knitr
-    ];
-    buildInputs = [pkgs.unstable.gsl];
-  };
-
   rstudio-with-packages = pkgs.unstable.rstudioWrapper.override {
     packages = with pkgs.unstable.rPackages; [
       tidyverse
-      # library(tidyverse) will load the core tidyverse packages:
-      ## ggplot2, for data visualisation.
-      ## dplyr, for data manipulation.
-      ## tidyr, for data tidying.
-      ## readr, for data import.
-      ## purrr, for functional programming.
-      ## tibble, for tibbles, a modern re-imagining of data frames.
-      ## stringr, for strings.
-      ## forcats, for factors.
-      ## lubridate, for date/times.
-
       tidymodels
       infer
-
       devtools
       remotes
-
       feather
       httr
       jsonlite
       xml2
       stringi
       curl
-
       shiny
-
       knitr
       rmarkdown
       tinytex
       ymlthis
-
       vegan
-
       ggtree
       ggtreeExtra
       tidytree
@@ -86,16 +34,13 @@ with lib.my; let
       MicrobiomeProfiler
       clusterProfiler
       enrichplot
-
       dada2
       DECIPHER
-
       ggpubr
       ggplotify
       ggalluvial
       ggstar
       ggnewscale
-
       coin
       forcats
       writexl
@@ -103,10 +48,8 @@ with lib.my; let
       randomForest
       curatedMetagenomicData
       SummarizedExperiment
-
       Rcpp
       Rcpp11
-
       Maaslin2
       pkgconfig
       ComplexHeatmap
@@ -116,7 +59,6 @@ with lib.my; let
       yaml
       optparse
       glue
-
       V8
       languageserver
     ];
