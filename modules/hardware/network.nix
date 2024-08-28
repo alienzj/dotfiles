e6e0943ff202b11451a8b33007201ef26a6f819e
Jie Zhu <alienchuj@gmail.com>
2024-08-28 09:46:53 +0800

    update flake.lock and network configuration

--- modules/hardware/network.nix
+++ modules/hardware/network.nix
@@ -74,33 +74,7 @@ in {
           enable = true;
           matchConfig.PermanentMACAddress = cfg.MACAddress;
           linkConfig.Name = "lan";
-	  networkCOnfig = {
-	    Bridge = "vmbr0";
-	  };
         };
-      };
-
-      #systemd.network.networks."10-lan" = {
-      #  matchConfig.Name = [ "ens18" ];
-      #  networkConfig = {
-      #    Bridge = "vmbr0";
-      #  };
-      #};
-
-      systemd.network.netdevs."vmbr0" = {
-        netdevConfig = {
-          Name = "vmbr0";
-          Kind = "bridge";
-        };
-      };
-
-      systemd.network.networks."10-lan-bridge" = {
-        matchConfig.Name = "vmbr0";
-          networkConfig = {
-            IPv6AcceptRA = true;
-            DHCP = "ipv4";
-          };
-          linkConfig.RequiredForOnline = "routable";
       };
     }
 
@@ -122,6 +96,26 @@ in {
           ntp = cfg.NetworkTimeServer;
 
           # make the routes on this interface a dependency for network-online.target
+          linkConfig.RequiredForOnline = "routable";
+
+          networkConfig = {
+            Bridge = "vmbr0";
+          };
+        };
+
+        netdevs."vmbr0" = {
+          netdevConfig = {
+            Name = "vmbr0";
+            Kind = "bridge";
+          };
+        };
+
+        networks."10-lan-bridge" = {
+          matchConfig.Name = "vmbr0";
+          networkConfig = {
+            IPv6AcceptRA = true;
+            DHCP = "ipv4";
+          };
           linkConfig.RequiredForOnline = "routable";
         };
       };
