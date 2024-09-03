8caa921f3627ff0bf06b05511c1b00c18ae6089c
Jie Zhu <alienchuj@gmail.com>
2024-09-03 23:21:41 +0800

    update eniac

--- hosts/eniac/default.nix
+++ hosts/eniac/default.nix
@@ -1,85 +1,154 @@
+# eniac -- my primary powerhouse
+## CPU: AMD Ryzen 7 1700X (16) @ 3.400GHz
+## GPU: NVIDIA GeForce GTX 1080 Ti
 {
-  pkgs,
-  config,
+  hey,
   lib,
   ...
-}: {
-  imports = [
-    ../home.nix
-    ./hardware-configuration.nix
-  ];
+}:
+with lib;
+with builtins; {
+  system = "x86_64-linux";
 
-  ## Modules
   modules = {
+    theme.active = "autumnal";
+    xdg.ssh.enable = true;
+
+    profiles = {
+      role = "workstation";
+      user = "alienzj";
+      networks = ["sz"];
+      hardware = [
+        "cpu/amd"
+        "gpu/nvidia"
+        "audio"
+        "audio/realtime"
+        "hidpi"
+        "ssd"
+      ];
+    };
+
     desktop = {
-      bspwm.enable = true;
-      hidpi.enable = true;
+      # X only
+      # bspwm.enable = true;
+      # term.default = "xst";
+      # term.st.enable = "true";
+
+      # Wayland only
+      hyprland = rec {
+        enable = true;
+        monitors = [
+          {
+            output = "HDMI-0";
+            position = "3840x2160";
+            primary = true;
+          }
+          #{ output = "DP-3";
+          #  position = "0x2191"; }
+          #{ output = "DP-2";
+          #  position = "4480x2191"; }
+          #{ output = "HDMI-A-1";
+          #  mode = "3840x2160@120";
+          #  position = "1280x0"; }
+        ];
+
+        extraConfig = ''
+          # REVIEW: Might be a hyprland bug, but an "Unknown-1" display is
+          #   always created and reserves some desktop space, so I disable it.
+          monitor = Unknown-1,disable
+
+          # Bind fixed workspaces to external monitors
+          ##workspace = name:left, monitor:DP-3, default:true
+          ##workspace = name:right, monitor:DP-2, default:true
+          ##workspace = name:tv, monitor:HDMI-A-1, default:true, gapsout:4
+
+          # Scroll by holding down a side button, because the wheel is broken
+          ##device {
+          ##    name = mosart-semi.-2.4g-wireless-mouse
+          ##    scroll_method = on_button_down
+          ##    scroll_button = 276
+          ##}
+
+          ##exec-once = hyprctl keyword monitor HDMI-A-1,disable
+        '';
+      };
+
+      term.default = "foot";
+      term.foot.enable = true;
+      term.wezterm.enable = true;
+
+      # Extra
       apps = {
-        net.enable = true;
         rofi.enable = true;
-        zoomus.enable = true;
+        filezilla.enable = true;
+        geph.enable = true;
+        godot.enable = true;
+        goldendict.enable = true;
         libreoffice.enable = true;
         wpsoffice.enable = true;
-        godot.enable = true;
-        thunderbird.enable = true;
-        transmission.enable = true;
-        filezilla.enable = true;
-        scrcpy.enable = true;
+        zoomus.enable = true;
         rustdesk.enable = true;
-        geph.enable = true;
-        rdp.enable = true;
-        goldendict.enable = true;
+        #teamviewer.enable = true;
+        #rdp.enable = true;
+        #net.enable = true;
+        scrcpy.enable = true;
+        thunar.enable = true;
+        #thunderbird.enable = true;
+        transmission.enable = true;
+        #ue.enable = true;
+        #unity3d.enable = true;
       };
+
       browsers = {
         default = "brave";
         brave.enable = true;
         firefox.enable = true;
-        qutebrowser.enable = true;
         chromium.enable = true;
+        #qutebrowser.enable = true;
       };
+
       gaming = {
-        steam.enable = true;
-        emulators = {
-          #psx.enable = true;
-          ds.enable = true;
-          gb.enable = false;
-          gba.enable = false;
-          snes.enable = false;
+        steam = {
+          enable = true;
+          #libraryDir = "/mnt/store/games/steam";
+          #libraryDir = "/media/windows/Program Files (x86)/Steam";
         };
-        games.enable = true;
+        #emulators = {
+        #psx.enable = true;
+        #ds.enable = true;
+        #gb.enable = false;
+        #gba.enable = false;
+        #snes.enable = false;
+        #};
+        #games.enable = true;
       };
+
       media = {
-        daw.enable = true;
-        documents = {
+        doc = {
           enable = true;
           pdf.enable = true;
           ebook.enable = true;
-          file.enable = true;
         };
-        graphics.enable = true;
-        mpv.enable = true;
-        vlc.enable = true;
-        youtube-tui.enable = true;
-        recording.enable = true;
-        spotify.enable = true;
-        sayonara.enable = true;
-        netease-cloud-music.enable = true;
-        lx-music.enable = true;
+        cad.enable = true;
+        daw.enable = true;
+        audio.enable = true;
+        video = {
+          enable = true;
+          capture.enable = true;
+          editor.enable = true;
+          player.enable = true;
+          tools.enable = true;
+        };
+        graphics = {
+          enable = true;
+          tools.enable = true;
+          raster.enable = true;
+          vector.enable = true;
+          sprites.enable = true;
+          design.enable = true;
+        };
       };
-      term = {
-        default = "xst";
-        st.enable = true;
-        wezterm.enable = true;
-        nnn.enable = true;
-      };
-      vm = {
-        lxd.enable = true;
-        qemu.enable = true;
-        virt-manager.enable = true;
-      };
-      noter = {
-        zotero.enable = true;
-      };
+
       im = {
         matrix.enable = true;
         tdesktop.enable = true;
@@ -87,26 +156,33 @@
         discord.enable = true;
         qqwechat.enable = true;
       };
+
+      vm = {
+        #osx.enable = true;
+        virt-manager.enable = true;
+        virtualbox.enable = true;
+      };
+
       input.fcitx5.enable = true;
+      noter.zotero.enable = true;
     };
+
     science = {
       ds.enable = true;
       ai.enable = true;
       cytoscape.enable = true;
       bioinfo.enable = true;
-      math = {
-        enable = true;
-        tools.enable = true;
-        wolframengine.enable = false;
-        mathematica.enable = false;
-        matlab.enable = true;
-      };
+      #math = {
+      #  enable = true;
+      #  tools.enable = true;
+      #  wolframengine.enable = false;
+      #  mathematica.enable = false;
+      #  matlab.enable = true;
+      #};
     };
+
     dev = {
-      cc = {
-        enable = true;
-        xdg.enable = true;
-      };
+      cc.enable = true;
       lua.enable = true;
       node = {
         enable = true;
@@ -149,32 +225,38 @@
       janet.enable = true;
       zig.enable = true;
     };
+
     editors = {
       default = "nvim";
       emacs = rec {
         enable = true;
-        doom = {
-          enable = false;
-          forgeUrl = "https://github.com";
-          repoUrl = "https://github.com/doomemacs/doomemacs";
-          configRepoUrl = "https://github.com/alienzj/doom.d";
-        };
+        #doom = {
+        #  enable = false;
+        #  forgeUrl = "https://github.com";
+        #  repoUrl = "https://github.com/doomemacs/doomemacs";
+        #  configRepoUrl = "https://github.com/alienzj/doom.d";
+        #};
       };
       vim.enable = true;
       vscode.enable = true;
-      vscodium.enable = false;
+      #vscodium.enable = true;
       rstudio.enable = true;
       android-studio.enable = true;
     };
+
     shell = {
+      #vaultwarden.enable = true;
+      #pass.enable = true;
       adl.enable = true;
       direnv.enable = true;
       git.enable = true;
       gnupg.enable = true;
       tmux.enable = true;
+      #zellij.enable = true;
       zsh.enable = true;
       fish.enable = true;
     };
+
     services = {
       adb.enable = true; # android
       syncthing.enable = true;
@@ -187,16 +269,13 @@
       ssh.enable = true;
       docker.enable = true;
       # Needed occasionally to help the parental units with PC problems
-      rdp.enable = true; # remote desktop
-      samba.enable = false; # share folders
-      printing = {
-        enable = true;
-        sharing = true;
-      };
-      lockscreen = {
-        enable = true;
-        inactiveInterval = 10;
-      };
+      #rdp.enable = true; # remote desktop
+      #samba.enable = true; # share folders
+      #printing.enable = true;
+      #lockscreen = {
+      #  enable = true;
+      #  inactiveInterval = 10;
+      #};
       flameshot = {
         enable = true;
         savePath = "/home/alienzj/pictures/flameshot";
@@ -221,32 +300,193 @@
         enable = true;
         configFile = "/home/alienzj/toolkits/ohconfig/rathole/pacman_eniac_c.toml";
       };
-      #rathole-client-awsman = {
-      #  enable = true;
-      #  configFile = "/home/alienzj/toolkits/ohconfig/rathole/awsman_eniac_c.toml";
-      #};
-      boinc.enable = false;
-      slurm.enable = false;
+      #boinc.enable = true;
+      #slurm.enable = true;
       earlyoom.enable = true;
-      home-assistant.enable = true;
-      onedrive.enable = false;
+      #home-assistant.enable = true;
+      #onedrive.enable = true;
     };
-    utils = {
-      sysinfo.enable = true;
-      traceroute.enable = true;
+
+    system = {
+      utils.enable = true;
+      fs.enable = true;
     };
-    theme.active = "alucard";
+
+    virt.qemu.enable = true;
   };
 
-  ## Local config
-  programs.ssh.startAgent = true;
-  services.openssh.startWhenNeeded = true;
-  services.pcscd.enable = true; # for gpg-agent
-  services.timesyncd.enable = true; # to sync time
+  ## local config
+  config = {pkgs, ...}: {
+    #networking.search = ["home.alienzj.tech"];
 
-  # Run unpatched dynamic binaries on NixOS
-  programs.nix-ld.enable = true;
+    # Low-latency audio for guitar recording and DAW stuff. Should not be
+    # generalized, since these values depend heavily on many local factors, like
+    # CPU speed, kernels, audio cards, etc.
+    services.pipewire.extraConfig.pipewire."95-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 64;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 64;
+      };
+    };
 
-  ## Security
-  #security.acme.defaults.email = "alienchuj@gmail.com";
+    user.packages = with pkgs; [
+      #unstable.guitarix
+      #gxplugins-lv2
+      #ladspaPlugins
+
+      signal-desktop
+    ];
+
+    ## Local config
+    #services.pcscd.enable = true; # for gpg-agent
+    #services.timesyncd.enable = true; # to sync time
+
+    # Run unpatched dynamic binaries on NixOS
+    programs.nix-ld.enable = true;
+
+    ## Security
+    #security.acme.defaults.email = "alienchuj@gmail.com";
+  };
+
+  hardware = {...}: {
+    # Kernel
+    ##boot = {
+    ##  initrd.availableKernelModules = [
+    ##    "xhci_pci"
+    ##    "vfio-pci"
+    ##    "ahci"
+    ##    "nvme"
+    ##    "usbhid"
+    ##    "usb_storage"
+    ##    "sd_mod"
+    ##    "sr_mod"
+    ##  ];
+    ##  initrd.kernelModules = [];
+    ##  kernelModules = [
+    ##    "tun"
+    ##    "vfio"
+    ##    "vfio_immmu_type1"
+    ##    "virtio"
+    ##    "vfio_pci"
+    ##    "kvm-amd"
+    ##    "acpi_call"
+    ##  ];
+
+    ##  kernelParams = [
+    ##    # didn't work
+    ##    #"usb.core.autosuspend=-1" # disable autosuspend
+    ##    #"usb.core.autosuspend=3600"  # 5 sencond # it seems no effect
+    ##  ];
+    ##
+    ##  extraModulePackages = with config.boot.kernelPackages; [acpi_call];
+    ##
+    ##  extraModprobeConfig = lib.mkMerge [
+    ##    "options kvm_amd nested=1"
+    ##    "options kvm_amd emulate_invalid_guest_state=0"
+    ##    "options kvm ignore_msrs=1"
+    ##    # idle audio card after one second
+    ##    #"options snd_hda_amd power_save=1"
+    ##    # enable wifi power saving (keep uapsd off to maintain low latencies)
+    ##    #"options iwlwifi power_save=1 uapsd_disable=1"
+    ##  ];
+    ##};
+
+    boot.supportedFilesystems = ["ntfs"];
+
+    #networking.interfaces.eno1.useDHCP = true;
+
+    # CPU
+    ##nix.settings = {
+    ##  cores = lib.mkDefault 12;
+    ##  max-jobs = lib.mkDefault 8;
+    ##};
+
+    # Disable all USB wakeup events to ensure restful sleep. This system has
+    # many peripherals attached to it (shared between Windows and Linux) that
+    # can unpredictably wake it otherwise.
+    systemd.services.fixSuspend = {
+      script = ''
+        for ev in $(grep enabled /proc/acpi/wakeup | cut --delimiter=\  --fields=1); do
+            echo $ev > /proc/acpi/wakeup
+        done
+      '';
+      wantedBy = ["multi-user.target"];
+    };
+
+    # services.xserver = {
+    #   # This must be done manually to ensure my screen spaces are arranged
+    #   # exactly as I need them to be *and* the correct monitor is "primary".
+    #   # Using xrandrHeads does not work.
+    #   monitorSection = ''
+    #     VendorName  "Unknown"
+    #     ModelName   "DELL U2515H"
+    #     HorizSync   30.0 - 113.0
+    #     VertRefresh 56.0 - 86.0
+    #     Option      "DPMS"
+    #   '';
+    #   screenSection = ''
+    #     Option "metamodes" "DP-5: nvidia-auto-select +0+31, HDMI-1: nvidia-auto-select +1920+0, DP-3: nvidia-auto-select +4480+31"
+    #     Option "SLI" "Off"
+    #     Option "MultiGPU" "Off"
+    #     Option "BaseMosaic" "off"
+    #     Option "Stereo" "0"
+    #     Option "nvidiaXineramaInfoOrder" "DFP-5"
+    #   '';
+    # };
+
+    # Displays
+    ##services.xserver = {
+    ##  enable = true;
+    ##  exportConfiguration = true;
+    ##  xkb.layout = "us";
+    ##  serverFlagsSection = ''
+    ##    Option "StandbyTime" "20"
+    ##    Option "SuspendTime" "30"
+    ##    Option "OffTime" "45"
+    ##    Option "BlankTime" "45"
+    ##  '';
+    ##};
+
+    ## Single monitor
+    ## https://github.com/NixOS/nixpkgs/issues/30796
+    ##services.xserver.displayManager.setupCommands = ''
+    ##  ${pkgs.xorg.xrandr}/bin/xrandr --dpi 168 --output HDMI-0 --mode 3840x2160 --rate 60 --pos 0x0 --primary
+    ##'';
+
+    # Mouse
+    #services.libinput = {
+    #  enable = true;
+    #  mouse.accelProfile = "flat";
+    #};
+
+    # Filesystem
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+      options = ["noatime" "errors=remount-ro"];
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-label/BOOT";
+      fsType = "vfat";
+    };
+
+    fileSystems."/home" = {
+      device = "/dev/disk/by-label/home";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+
+    fileSystems."/mnt/store" = {
+      device = "/dev/disk/by-label/store";
+      fsType = "ext4";
+    };
+
+    swapDevices = [{device = "/dev/disk/by-label/swap";}];
+
+    # User
+    user.extraGroups = ["tss" "video"];
+  };
 }
