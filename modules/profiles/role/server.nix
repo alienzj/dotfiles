40513a06ece500618ddf35758787cb29b7316e97
Jie Zhu <alienchuj@gmail.com>
2024-09-06 13:44:21 +0800

    adjust hardware

--- modules/profiles/role/server.nix
+++ modules/profiles/role/server.nix
@@ -32,6 +32,42 @@ with lib;
       };
     };
 
+    ### https://github.com/nix-community/srvos/blob/main/nixos/server/default.nix
+    #systemd = {
+    #  # Given that our systems are headless, emergency mode is useless.
+    #  # We prefer the system to attempt to continue booting so
+    #  # that we can hopefully still access it remotely.
+    #  enableEmergencyMode = false;
+
+    #  # For more detail, see:
+    #  #   https://0pointer.de/blog/projects/watchdog.html
+    #  watchdog = {
+    #    # systemd will send a signal to the hardware watchdog at half
+    #    # the interval defined here, so every 10s.
+    #    # If the hardware watchdog does not get a signal for 20s,
+    #    # it will forcefully reboot the system.
+    #    runtimeTime = "20s";
+    #    # Forcefully reboot if the final stage of the reboot
+    #    # hangs without progress for more than 30s.
+    #    # For more info, see:
+    #    #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
+    #    rebootTime = "30s";
+    #  };
+
+    #  sleep.extraConfig = ''
+    #    AllowSuspend=no
+    #    AllowHibernation=no
+    #  '';
+    #};
+
+    ## TODO
+    ## https://wiki.archlinux.org/title/Wake-on-LAN
+
+    ## TODO
+    ## http://blog.lujun9972.win/blog/2018/12/31/%E4%BD%BF%E7%94%A8rtcwake%E5%AE%9A%E6%97%B6%E5%94%A4%E9%86%92linux/index.html
+    ## Sleep in time
+    ## Wake up in time
+
     powerManagement.cpuFreqGovernor = mkDefault "ondemand";
     virtualisation.docker.enableOnBoot = mkDefault true;
     power.ups.mode = mkDefault "netclient";
@@ -41,3 +77,28 @@ with lib;
     # Prevent replacing the running kernel w/o reboot
     security.protectKernelImage = true;
   }
+# Summary
+## reference
+## http://blog.lujun9972.win/blog/2018/06/21/linux%E5%AE%9A%E6%97%B6%E4%BC%91%E7%9C%A0/
+## 目前大概由三种类型的休眠:
+### suspend(suspend to RAM)
+### 指的是除了内存以外的大部分机器部件都进入断电状态
+### 这种休眠状态恢复速度特别快，但由于内存中的数据并没有被保存下来
+### 因此这个状态的系统并没有进入真正意义上的休眠状态，还在持续耗电
+### hibernate(suspend to disk)
+### 这种休眠会将内存中的系统状态写入交换空间内
+### 当系统启动时就可以从交换空间内读回系统状态
+### 这种情况下系统可以完全断电,但由于要保存/读取系统状态到/从交换空间，因此速度会比较慢，而且需要进行一些配置
+### hybrid(suspend to both)
+### 结合了上面两种休眠类型
+### 它像 hibernate 一样将系统状态存入交换空间内
+### 同时也像 suspend 一样并不关闭电源
+### 这种，在电源未耗尽之前，它能很快的从休眠状态恢复
+### 而若休眠期间电源耗尽，则它可以从交换空间中恢复系统状态
+# Finally
+## man systemd-sleep
+### systemd-suspend.service
+### systemd-hibernate.service
+### systemd-hybrid-sleep.service
+### /usr/lib/systemd/system-sleep
+
--- modules/profiles/role/workstation.nix
+++ modules/profiles/role/workstation.nix
@@ -116,3 +116,161 @@ with hey.lib;
 
     # ...
   ])
+# reference
+## networking.useNetworkd
+### https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/tasks/network-interfaces.nix
+### Whether we should use networkd as the network configuration backend or
+### the legacy script based system. Note that this option is experimental,
+### enable at your own risk.
+## networking.networkmanager.enable ??
+### https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/networking/networkmanager.nix
+### Whether to use NetworkManager to obtain an IP address and other
+### configuration for all network interfaces that are not manually
+### configured. If enabled, a group `networkmanager`
+### will be created. Add all users that should have permission
+### to change network settings to this group.
+## systemd-networkd
+### https://wiki.nixos.org/wiki/Systemd/networkd
+#  {
+#    hey,
+#    lib,
+#    options,
+#    config,
+#    pkgs,
+#    ...
+#  }:
+#    with lib;
+#    with hey.lib; let
+#      cfg = config.modules.hardware.network;
+#    in {
+#      options.modules.hardware.network = with types; {
+#        enable = mkBoolOpt false;
+#        networkd.enable = mkBoolOpt false;
+#        networkmanager.enable = mkBoolOpt false;
+#        wireless.enable = mkBoolOpt false;
+#        MACAddress = mkOpt types.str "10:7b:44:8e:fe:b4";
+#        IPAddress = mkOption {
+#          type = with types; listOf types.str;
+#          default = [""];
+#        };
+#        RouteGateway = mkOption {
+#          type = with types; listOf types.str;
+#          default = [""];
+#        };
+#        DomainNameServer = mkOption {
+#          type = with types; listOf types.str;
+#          default = ["223.5.5.5" "8.8.8.8" "119.29.29.29"];
+#        };
+#        NetworkTimeServer = mkOption {
+#          type = with types; listOf types.str;
+#          default = ["ntp7.aliyun.com" "ntp.aliyun.com"];
+#        };
+#      };
+#
+#      config = mkIf cfg.enable (mkMerge [
+#        {
+#          # Firewall
+#          ### https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/networking/firewall.nix
+#          ### Whether to enable the firewall.  This is a simple stateful
+#          ### firewall that blocks connection attempts to unauthorised TCP
+#          ### or UDP ports on this machine.
+#          networking = {
+#            # Docker and libvirt use iptables
+#            nftables.enable = false;
+#            firewall = {
+#              enable = true;
+#              allowPing = true;
+#              pingLimit = "--limit 1/minute --limit-burst 5";
+#              allowedTCPPorts = [22 80 443 3389 8080];
+#              allowedUDPPorts = [22 80 443 3389 8080];
+#            };
+#          };
+#
+#          # rename interface
+#          systemd.network = {
+#            ## https://nixos.org/manual/nixos/stable/#sec-rename-ifs
+#            links."10-lan" = {
+#              enable = true;
+#              matchConfig.PermanentMACAddress = cfg.MACAddress;
+#              linkConfig.Name = "lan";
+#            };
+#          };
+#        }
+#
+#        # used in ether network
+#        (mkIf cfg.networkd.enable {
+#          systemd.network = {
+#            ### whether to enable networkd or not
+#            enable = true;
+#            ### no need for ether network
+#            wait-online.enable = false;
+#
+#            networks."10-lan" = {
+#              enable = true;
+#              matchConfig.Name = "lan";
+#              matchConfig.Type = "ether";
+#              address = cfg.IPAddress;
+#              gateway = cfg.RouteGateway;
+#              dns = cfg.DomainNameServer;
+#              ntp = cfg.NetworkTimeServer;
+#
+#              # make the routes on this interface a dependency for network-online.target
+#              linkConfig.RequiredForOnline = "routable";
+#
+#              networkConfig = {
+#                Bridge = "vmbr0";
+#              };
+#            };
+#
+#            netdevs."vmbr0" = {
+#              netdevConfig = {
+#                Name = "vmbr0";
+#                Kind = "bridge";
+#              };
+#            };
+#
+#            networks."10-lan-bridge" = {
+#              matchConfig.Name = "vmbr0";
+#              networkConfig = {
+#                IPv6AcceptRA = true;
+#                DHCP = "ipv4";
+#              };
+#              linkConfig.RequiredForOnline = "routable";
+#            };
+#          };
+#        })
+#
+#        # used in wlan network
+#        (mkIf cfg.wireless.enable {
+#          system.activationScripts = {
+#            rfkillUnblockWlan = {
+#              text = ''
+#                rfkill unblock wlan
+#              '';
+#              deps = [];
+#            };
+#          };
+#        })
+#
+#        # used in wlan network
+#        (mkIf cfg.networkmanager.enable {
+#          networking = {
+#            interfaces.lan.useDHCP = true;
+#            networkmanager = {
+#              enable = true;
+#              plugins = with pkgs; [
+#                networkmanager-fortisslvpn
+#                networkmanager-iodine
+#                networkmanager-l2tp
+#                networkmanager-openconnect
+#                networkmanager-openvpn
+#                networkmanager-vpnc
+#                networkmanager-sstp
+#              ];
+#            };
+#            nameservers = cfg.DomainNameServer;
+#          };
+#        })
+#      ]);
+#    }
+
