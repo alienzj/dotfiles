2f4308aa1c14656cee9d869dcd90eaa4c9f9129a
Jie Zhu <alienchuj@gmail.com>
2024-09-06 18:53:52 +0800

    checking

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 export http_proxy=http://127.0.0.1:9910
 export https_proxy=http://127.0.0.1:9910
 
-export HEYENV="{\"user\":\"alienzj\",\"host\":\"eniac\",\"flake\":\"/etc/dotfiles\",\"theme\":\"autumnal\"}"
+export HEYENV="{\"user\":\"alienzj\",\"host\":\"eniac\",\"flake\":\"/etc/dotfiles\",\"theme\":\"autumnal\",\"path\":\"/etc/dotfiles\"}"
 
 nix flake check \
 	--impure \
--- modules/profiles/hardware/hidpi.nix
+++ modules/profiles/hardware/hidpi.nix
@@ -15,28 +15,32 @@ with hey.lib;
     #  QT_AUTO_SCREEN_SCALE_FACTOR = "true";
     #};
 
-    services.xserver.dpi = 168;
+    #services.xserver.dpi = 168;
 
     # reference
     # https://wiki.archlinuxcn.org/zh-hans/HiDPI
+    # https://github.com/hlissner/dotfiles/blob/master/modules/profiles/hardware/hidpi.nix
     environment.variables = {
+      QT_DEVICE_PIXEL_RATIO = "2";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "true";
+
       # QT method: manually
       ##QT_SCALE_FACTOR = "2";
-      QT_SCREEN_SCALE_FACTORS = "2;2";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "0";
+      #QT_SCREEN_SCALE_FACTORS = "2;2";
+      #QT_AUTO_SCREEN_SCALE_FACTOR = "0";
 
       # QT method: automatically
       #QT_AUTO_SCREEN_SCALE_FACTOR = "1";
 
       # GTK
-      GDK_SCALE = "2";
-      GDK_DPI_SCALE = "0.5";
-      _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+      #GDK_SCALE = "2";
+      #GDK_DPI_SCALE = "0.5";
+      #_JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
     };
 
     # link recursively so other modules can link files in their folders
     #Xcursor.size: 32
-    home.configFile."xtheme/80-high-dpi".text = ''
-      Xft.dpi: 168
-    '';
+    #home.configFile."xtheme/80-high-dpi".text = ''
+    #  Xft.dpi: 168
+    #'';
   }
